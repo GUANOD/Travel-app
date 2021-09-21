1b030c4c1c01bd890bf59021b11458e18baae8d7
GUANOD <guano.carneiro@outlook.com>
2021-09-21 23:01:56 +0200

    add button

--- src/cart.js
+++ src/cart.js
@@ -1,43 +1,65 @@
-import React, { Component } from 'react'
-import './style/cart.css'
-import chevUp from './media/icons/svg/up-chevron.svg'
-import chevDown from './media/icons/svg/down-chevron.svg'
+import React, { Component } from "react";
+import "./style/cart.css";
+import chevUp from "./media/icons/svg/up-chevron.svg";
+import chevDown from "./media/icons/svg/down-chevron.svg";
 
-
-function CartItem(props){
-    let {pic, title, desc, price, quantity} = props.item;
-    return(
-        <div className="item">
-            <div className="pic">
-                <img src={pic} alt=""/>
-            </div>
-            <div className="info">
-                <h3 className="tittle">{title}</h3>
-                <p className="desc">{desc}</p>
-                <p onClick={()=> props.handleShow()} className="price">{"$" + parseInt(price) * parseInt(quantity)}</p>
-            </div>
-            <div className="changeQuant">
-                <img onClick={()=> props.changeQuant(props.index, true)}  className="chevron" src={chevUp} alt=""/>
-                <p>{quantity}</p>
-                <img onClick={()=> props.changeQuant(props.index, false)} className="chevron" src={chevDown} alt=""/>
-            </div>
-        </div>
-    )
+function CartItem(props) {
+  let { pic, title, desc, price, quantity } = props.item;
+  return (
+    <div className="item">
+      <div className="pic">
+        <img src={pic} alt="" />
+      </div>
+      <div className="info">
+        <h3 className="tittle">{title}</h3>
+        <p className="desc">{desc}</p>
+        <p className="price">{"$" + parseInt(price) * parseInt(quantity)}</p>
+      </div>
+      <div className="changeQuant">
+        <img
+          onClick={() => props.changeQuant(props.index, true)}
+          className="chevron"
+          src={chevUp}
+          alt=""
+        />
+        <p>{quantity}</p>
+        <img
+          onClick={() => props.changeQuant(props.index, false)}
+          className="chevron"
+          src={chevDown}
+          alt=""
+        />
+      </div>
+    </div>
+  );
 }
 
 export default class Cart extends Component {
-    render() {
-        return (
-            <div className={this.props.show + " cartContainer"}>
-                <div className="cart">
-                    {this.props.cart.length === 0 && <div onClick={this.props.handleShow} className="noItems"><p>No items in Cart.</p><p> Keep on shopping!</p></div>}
-                    {this.props.cart.map((item,index) => {
-                        return(
-                            <CartItem handleShow={this.props.handleShow} index={index} key={index} item={item} changeQuant={this.props.changeQuant} />
-                        )
-                        })}
-                </div>
+  render() {
+    return (
+      <div className={this.props.show + " cartContainer"}>
+        <div className="cart">
+          {this.props.cart.length === 0 && (
+            <div onClick={this.props.handleShow} className="noItems">
+              <p>No items in Cart.</p>
+              <p> Keep on shopping!</p>
             </div>
-        )
-    }
+          )}
+          {this.props.cart.map((item, index) => {
+            return (
+              <CartItem
+                index={index}
+                key={index}
+                item={item}
+                changeQuant={this.props.changeQuant}
+              />
+            );
+          })}
+          <button className="xButton" onClick={() => this.props.handleShow()}>
+            X
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
